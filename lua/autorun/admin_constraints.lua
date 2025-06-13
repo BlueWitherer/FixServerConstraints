7485153ae248c24b6f6aa2776492336547f1f0b4
Cheeseworks <bluewitherer2422@gmail.com>
2025-06-13 09:09:01 -0500

    fix nil vars

--- lua/autorun/admin_constraints.lua
+++ lua/autorun/admin_constraints.lua
@@ -3,10 +3,12 @@ if SERVER then -- Client-side only
     return
 elseif CLIENT then
     local function CheckAdmin() -- Check if the player has permission to modify constraint limits
+        local ply = LocalPlayer()
+        if not IsValid(ply) or not ply.IsAdmin then return false end
         local RestrictToSuperAdmin = GetConVar("welds_superadminonly"):GetBool() -- Check if superadmin restriction is enabled
         if RestrictToSuperAdmin then
             print("Superadmin restriction is enabled, checking player status")
-            if LocalPlayer():IsSuperAdmin() then
+            if ply:IsSuperAdmin() then
                 print("Player is a superadmin, permission granted")
                 return true -- Superadmin has permission
             else
@@ -14,7 +16,7 @@ elseif CLIENT then
                 return false -- Non-superadmin does not have permission
             end
         else
-            if LocalPlayer():IsAdmin() then
+            if ply:IsAdmin() then
                 print("Player is an admin, permission granted")
                 return true -- Admin has permission
             else
@@ -25,12 +27,14 @@ elseif CLIENT then
     end
 
     local function AdminConstraintSlider(pnl) -- Add a slider to update constraint limits
+        local ply = LocalPlayer()
+        if not IsValid(ply) or not ply.IsAdmin then return false end
         if CheckAdmin() then -- Check for admin
             print("Player has permission to modify constraint limits")
             pnl:Help("Adjust the maximum number of constraints allowed on the server.")
             pnl:NumSlider("Max Constraints", "sbox_maxconstraints", 100, 2000, 0)
             pnl:NumSlider("Max Rope Constraints", "sbox_maxropeconstraints", 100, 2000, 0)
-            if LocalPlayer():IsSuperAdmin() then -- Check if the player is a superadmin
+            if ply:IsSuperAdmin() then -- Check if the player is a superadmin
                 print("Superadmin detected, adding superadmin-only constraint permission setting")
                 pnl:Help("You're a superadmin, you can restrict constraint limit modifications to superadmins only.")
                 pnl:CheckBox("Restrict to Super-Admins", "welds_superadminonly")

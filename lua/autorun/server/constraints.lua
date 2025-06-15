85cc83ce8a67dc99c27952e50ca36ba7370706e7
Cheeseworks <bluewitherer2422@gmail.com>
2025-06-15 09:11:19 -0500

    debug convar

--- lua/autorun/server/constraints.lua
+++ lua/autorun/server/constraints.lua
@@ -6,8 +6,9 @@ if SERVER then
     CreateConVar("sbox_maxropeconstraints", 1000, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Max rope constraints limit")
     FscPrint.debug("Creating constraint permission convar...")
     CreateConVar("welds_superadminonly", 1, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Restrict constraint limit modifications to superadmins only")
+    -- testing
     FscPrint.debug("Creating debug convar...")
-    CreateConVar("test_cheeseworks", 65, FCVAR_ARCHIVE, "Cheeseworks test convar") -- debug
+    CreateConVar("test_cheeseworks", 65, {FCVAR_ARCHIVE}, "Cheeseworks test convar")
     FscPrint.log("Created all convars for constraint limit addon!")
     return
 else

873e8562103b73194d28a4108649ec0448ffeeea
Cheeseworks <bluewitherer2422@gmail.com>
2025-06-18 15:08:38 -0500

    fix perm event

--- lua/autorun/client/spawnmenu.lua
+++ lua/autorun/client/spawnmenu.lua
@@ -12,6 +12,7 @@ if CLIENT then -- Client-side only
         log:debug("Checking if player has admin...")
         local ply = LocalPlayer()
         if IsValid(ply) then -- Check if player object is valid
+            log:debug("Player", ply:Nick(), "is valid, checking permissions...")
             local AdminBool = GetGlobal2Bool(vars.adminperm.name, true) -- Check if superadmin restriction is enabled
             if AdminBool then
                 log:debug("Superadmin restriction is enabled, checking player status")
@@ -37,36 +38,33 @@ if CLIENT then -- Client-side only
         end
     end
 
-    local SendVarUpdate = function(cvar, value)
-        -- Send a convar update to the server
-        log:debug("Sending convar update to server for", cvar, "with value", value)
-        net.Start("FSC_SetConstraintConVar")
-        net.WriteString(cvar)
-        net.WriteFloat(value)
-        net.SendToServer()
-    end
-
     local ConstraintSettings = function(pnl)
         -- Add a slider to update constraint limits
         log:debug("Setting up constraint limit sliders to spawnmenu utilities...")
         local ply = LocalPlayer()
         if IsValid(ply) then -- Check if player object is valid
+            log:debug("Player", ply:Nick(), "is valid, checking permissions...")
             if CheckAdmin() then -- Check for admin
                 log:info("Player has permission to modify constraint limits")
                 pnl:Help("Adjust the maximum number of constraints allowed on the server.")
                 local weldSlider = pnl:NumSlider("Max Constraints", vars.maxwelds.name, 10, 2000, 0)
                 local ropeSlider = pnl:NumSlider("Max Rope Constraints", vars.maxropes.name, 10, 2000, 0)
-                pnl:Help("If you're on a server, you likely cannot update the values as you adjust the slider. To fix this, press the button below to manually update the constraint limits. This is not required if you're hosting this server on your computer.")
+                pnl:Help("If you're on a server, you likely cannot update the values in realtime as you adjust the slider. To fix this, press the button below to manually update the constraint limits. This is not required if you're hosting this server on your computer.")
                 local UpdateBtn = pnl:Button("Update Limits")
                 UpdateBtn.DoClick = function()
                     -- Update button clicked callback
                     log:debug("Update button clicked, sending new constraint limits to server")
                     local weldValue = weldSlider:GetValue()
                     local ropeValue = ropeSlider:GetValue()
-                    net.Start("FSC_SetConstraintConVars")
-                    net.WriteFloat(weldValue)
-                    net.WriteFloat(ropeValue)
-                    net.SendToServer()
+                    local start = net.Start("FSC_SetConstraintConVars")
+                    if start then
+                        net.WriteFloat(weldValue)
+                        net.WriteFloat(ropeValue)
+                        net.SendToServer()
+                    else
+                        log:error("Failed to start net message for updating constraint limits")
+                        return
+                    end
                 end
 
                 pnl:Help("Reset the constraint limits to their default values.")
@@ -74,8 +72,13 @@ if CLIENT then -- Client-side only
                 ResetBtn.DoClick = function()
                     -- Reset button clicked callback
                     log:debug("Reset button clicked, resetting constraint limits to default")
-                    net.Start("FSC_ResetConstraintConVars")
-                    net.SendToServer()
+                    local start = net.Start("FSC_ResetConstraintConVars")
+                    if start then
+                        net.SendToServer()
+                    else
+                        log:error("Failed to start net message for resetting constraint limits")
+                        return
+                    end
                 end
             else
                 log:warn("Player does not have permission to modify constraint limits")
@@ -100,6 +103,7 @@ if CLIENT then -- Client-side only
         log:debug("Setting up constraint admin to spawnmenu options...")
         local ply = LocalPlayer()
         if IsValid(ply) then
+            log:debug("Player", ply:Nick(), "is valid, checking permissions...")
             if ply:IsSuperAdmin() then -- Check if the player is a superadmin
                 log:info("Superadmin detected, adding superadmin-only constraint permission setting")
                 pnl:Help("You're a superadmin, you can restrict constraint limit modifications to superadmins only.")
@@ -111,7 +115,14 @@ if CLIENT then -- Client-side only
                     log:debug("Update button pressed, sending superadmin restriction update to server")
                     local isChecked = adminCheckBox:GetChecked()
                     log:info("Superadmin restriction is now", isChecked and "enabled" or "disabled")
-                    SendVarUpdate(vars.adminperm.name, isChecked and 1 or 0)
+                    local start = net.Start("FSC_SetConstraintAdmin")
+                    if start then
+                        net.WriteBool(isChecked)
+                        net.SendToServer()
+                    else
+                        log:error("Failed to start net message for updating superadmin restriction")
+                        return
+                    end
                 end
             else
                 log:warn("Player does not have permission to modify constraint permissions")
@@ -137,6 +148,7 @@ if CLIENT then -- Client-side only
         log:print("Hooked spawnmenu settings")
         local ply = LocalPlayer()
         if IsValid(ply) then
+            log:debug("Player", ply:Nick(), "is valid, checking permissions...")
             if ply:IsSuperAdmin() then
                 log:debug("Player", ply:Nick(), "is a superadmin")
             elseif ply:IsAdmin() then
